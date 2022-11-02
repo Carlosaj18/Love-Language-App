c8f3d97f72cb295da681999d903b443e36ae24e2
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-11-02 18:10:41 -0500

    Arreglar app.js

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ const PopUpEditUser            = document.querySelector(".modal-body.editUser");
 const closeEditButton          = document.getElementById("close-button-edit");
 
                                           /*** SEARCH BAR SECTION */
-
 // Evento click search bar & keydown
 searchContainer.addEventListener("click", () => {
   usersLoad();
@@ -23,56 +22,61 @@ searchContainer.addEventListener("click", () => {
 });
 
                                           /*** DASHBOARD SECTION **/
-
 // Evento click Dashboard
 buttonDashboard.addEventListener("click", () => {
   containerDashboard.style.display = "block";
-  let userLocals = recuperarUsers();
-  displayLenguajesDelAmor(userLocals);
+  displayLenguajesDelAmor(recuperarUsers());
   ordenarDashboard();
 });
 
-                                      /*** USER FAVORITES SECTION **/ 
-
+                                         /*** USER FAVORITES SECTION **/ 
 // Evento click boton Favoritos
 favoritosUsers.addEventListener("click", () => {
   userFavoriteLoad();
-  if(usersFavorite.length <= 0 ){
-    alertaErrorUsuarios("error", "No hay usuarios en favoritos");
-  }
-  containerDashboard.style.display = "none";
+  if(usersFavorite.length <= 0) { return alertaErrorUsuarios("error", "No hay usuarios en favoritos") };
+  containerDashboardLoad();
 });
 
-                                            /*** FIND USER PROMT SECTION **/
-
+                                          /*** FIND USER PROMT SECTION **/
 // Evento click boton Find User prompt
 findUser.addEventListener("click", () => {
   findUserArray();
-  containerDashboard.style.display = "none";
+  containerDashboardLoad();
 });
 
-                                            /*** ADD USER SECTION **/                                          
+                                          /*** ADD USER SECTION **/                                          
 // Evento click boton Crear User
 addUser.addEventListener("click", () => activarPopUpForm());
 
-                                            /*** ALL USERS SECTION **/
-   
+                                          /*** ALL USERS SECTION **/   
  // Evento click boton Ordenar
- buttonOrdenar.addEventListener("click", () => recuperarUsers() ? ordenar(recuperarUsers()) + alertaOrdenarDashboard() :  alertaErrorUsuarios("warning", `No se encontró usuarios`));
+buttonOrdenar.addEventListener("click", () => recuperarUsers() ? ordenar(recuperarUsers()) + alertaOrdenarDashboard() :  alertaErrorUsuarios("warning", `No se encontró usuarios`));
 
 // Evento click boton All Users
 allUsers.addEventListener("click", () => usersLoad()); 
 
-
-                                            /*** LOAD WINDOW **/
-window.addEventListener("load", (event) => {
+                                          /*** LOAD WINDOW **/
+// Container Dashboard loading                                             
+const containerDashboardLoad = () => {
+  const containerDashboard         = document.querySelector(".container-dashboard");
   containerDashboard.style.display = "none";
-  modal.style.display = "none";
-  activarPopUpForm();
-  const displayLanguage = document.querySelector(".modalLanguage");
+}
+
+// PopuUps loading 
+const popUpsLoad = () => {
+  const displayLanguage         = document.querySelector(".modalLanguage");
+  const PopUpEditUser           = document.querySelector(".modalEditUser");
+  const modal                   = document.querySelector(".modal");
   displayLanguage.style.display = "none";
-  const PopUpEditUser      = document.querySelector(".modalEditUser");
-  PopUpEditUser.style.display = "none";
+  PopUpEditUser.style.display   = "none";
+  modal.style.display           = "none";
+}
+
+// Window loading 
+window.addEventListener("load", () => {
+  popUpsLoad();
+  containerDashboardLoad();
+  activarPopUpForm();
 });
 
 // See localStorage Keys
